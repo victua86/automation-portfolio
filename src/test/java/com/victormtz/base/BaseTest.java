28735ccccfde008e5dea975272080ce14c252be8
Victor Martinez <victor.martinez.86@outlook.com>
2026-06-01 16:57:59 -0600

    Files ADDED and MODIFIED

--- src/test/java/com/victormtz/base/BaseTest.java
+++ src/test/java/com/victormtz/base/BaseTest.java
@@ -7,6 +7,9 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class BaseTest {
 
     protected WebDriver driver;
@@ -18,6 +21,13 @@ public class BaseTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-maximized");
         options.addArguments("--disable-notifications");
+        options.addArguments("--disable-save-password-bubble");
+        options.addArguments("--password-store=basic");
+
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("credentials_enable_service", false);
+        prefs.put("profile.password_manager_enabled", false);
+        options.setExperimentalOption("prefs", prefs);
 
         driver = new ChromeDriver(options);
         driver.get("https://www.saucedemo.com");
